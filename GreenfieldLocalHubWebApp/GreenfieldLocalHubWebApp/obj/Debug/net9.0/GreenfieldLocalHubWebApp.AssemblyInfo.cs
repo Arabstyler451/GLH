e2fc42e49d9babdab2a47962db82e349183db7b9
Arabstyler451 <0612abna@gmail.com>
2026-04-23 23:21:47 +0100

    save

--- GreenfieldLocalHubWebApp/GreenfieldLocalHubWebApp/obj/Debug/net9.0/GreenfieldLocalHubWebApp.AssemblyInfo.cs
+++ GreenfieldLocalHubWebApp/GreenfieldLocalHubWebApp/obj/Debug/net9.0/GreenfieldLocalHubWebApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreenfieldLocalHubWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eb41093689707f4270bebe1f67079469fdfba81")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreenfieldLocalHubWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreenfieldLocalHubWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
